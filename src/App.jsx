d2d2681f0f5f39e2a05b75e86107c072cada48d2
donchony <88427187+donchony@users.noreply.github.com>
2024-09-16 21:37:28 -0400

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
   return (
